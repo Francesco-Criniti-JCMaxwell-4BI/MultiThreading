28186c044b28971bf6fbaeecd9458b0070acce29
Francesco-Criniti-JCMaxwell-4BI <francesco.criniti@jcmaxwell.it>
2017-04-28 20:02:00 +0200

    Update MultiThread.java

--- MultiThread.java
+++ MultiThread.java
@@ -2,6 +2,11 @@ package multithread;
 import java.util.concurrent.TimeUnit;
 import static multithread.TicTacToe.cont;
 
+/**
+ *
+ * @author Francesco Criniti
+ */
+
 public class ES1 
 {
     // "main" e' il THREAD principale da cui vengono creati e avviati tutti gli altri THREADs
@@ -26,28 +31,28 @@ public class ES1
         long end = System.currentTimeMillis();
         System.out.println("Main Thread completata! tempo di esecuzione: " + (end - start) + "ms");
         
-        try 
+        try //qui' vengono gestite le eccezioni
         {
             tic.join();
         }     
         catch (InterruptedException e) 
         {}
     
-        try 
+        try //qui' vengono gestite le eccezioni
         {
             tac.join();
         } 
         catch (InterruptedException e) 
         {}
         
-        try 
+        try //qui' vengono gestite le eccezioni
         {
             toe.join();
         } 
         catch (InterruptedException e) 
         {}
         System.out.println();
-        System.out.println("punteggio: " + cont);
+        System.out.println("punteggio: " + cont);  //stampo il punteggio
     }
 }
 // Ci sono vari (troppi) metodi per creare un THREAD in Java questo e' il mio preferito per i vantaggi che offre
@@ -55,11 +60,12 @@ public class ES1
 // +1 si possono passare parametri (usando il Costruttore)
 // +1 si puo' controllare quando un THREAD inizia indipendentemente da quando e' stato creato
 class TicTacToe implements Runnable 
-{    
+{   
+     // non essendo "static" c'e' una copia delle seguenti variabili per ogni THREAD
     private String t;
     private String msg;
+    //essendo una variabile static ne basta una per tutti i THREAD
     public static int cont = 0;
-
     public static boolean c = false;
     // Costruttore, possiamo usare il costruttore per passare dei parametri al THREAD
     public TicTacToe (String s) 
@@ -73,16 +79,16 @@ class TicTacToe implements Runnable
 
     public void run() 
     {
-        for (int i = 10; i > 0; i--) 
+        for (int i = 10; i > 0; i--) //ciclo del conto alla rovescia
         {           
             if("TAC".equals(t))
                 c = true;
                 
             msg = "<" + t + "> ";
             //System.out.print(msg);
-            int casuale=100+(int)(Math.random()*300);
+            int casuale=100+(int)(Math.random()*300); //genera un numero casuale da 100 a 300
              try {
-                TimeUnit.MILLISECONDS.sleep(casuale);
+                TimeUnit.MILLISECONDS.sleep(casuale);  //il numero casuale diventa il tempo con cui viene effettuato il conto alla rovescia
             } catch (InterruptedException e) {}
             if("TOE".equals(t) && c == true)
                 cont++;
